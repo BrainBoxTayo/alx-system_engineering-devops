b9016c7388e94c9f4fd85103f73e5826a4a44168
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2025-03-24 20:17:09 +0100

    puppet practice

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+node default {
+  file{'/home/ubuntu/.ssh/config':
+  ensure => 'present',
+  content => "
+	Host ubuntu
+		HostName 3.87.30.11
+		User user
+		IdentityFile ~/.ssh/school
+		PasswordAuthentication no",
+  owner => 'ubuntu',
+  group => 'ubuntu',
+  mode => '0666',
+  }
+}
